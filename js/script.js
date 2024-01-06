9b7ebb51ed537cab502e7380e3ad6e7726841fd7
tnayem <tasdidnayem@gmail.com>
2024-01-07 01:38:56 +0600

    card section added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 document.getElementById('checkAge').addEventListener('click',function(){
     const ageValue = document.getElementById('ageValue');
     const ageValueNumber = Number(ageValue.value)
-    const validInput = document.getElementById('validInput')
-    const modal = document.getElementById('my_modal_5')
     if(ageValueNumber===0){
         
-        validInput.classList.remove("hidden")
+        validInput.showModal()
     }else{
-        validInput.classList.add("hidden")
         if(ageValueNumber>=18){
             modalOne.showModal()
         }else{
